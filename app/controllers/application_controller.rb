7631dfedb0f6931e3fb4d981975a47d33e8d6779
Marco Castro <marcodotcastro@gmail.com>
2025-06-03 21:00:28 -0300

    style: cadastro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
   layout :layout_by_resource
 
   private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :employee_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :employee_id])
+  end
 
   def layout_by_resource
     if devise_controller?
