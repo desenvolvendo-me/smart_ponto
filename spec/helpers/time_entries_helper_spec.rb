18c5bff7bc772a9a35420434ae04bf70ddbbc223
Marco Castro <marcodotcastro@gmail.com>
2026-05-16 11:34:06 -0300

    feat: allow managers to register weekend time entries

--- spec/helpers/time_entries_helper_spec.rb
+++ spec/helpers/time_entries_helper_spec.rb
@@ -1,15 +1,45 @@
 require 'rails_helper'
-
-# Specs in this file have access to a helper object that includes
-# the TimeEntriesHelper. For example:
-#
-# describe TimeEntriesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 RSpec.describe TimeEntriesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#can_register_on_date?' do
+    let(:weekend_date) { Date.new(2026, 5, 17) }
+
+    it 'returns true for gestor even without explicit release' do
+      user = User.new(role: 'gestor', weekend_registration_allowed: false)
+
+      expect(helper.can_register_on_date?(user, weekend_date)).to be(true)
+    end
+
+    it 'returns false for non-manager without explicit release' do
+      user = User.new(role: 'funcionario', weekend_registration_allowed: false)
+
+      expect(helper.can_register_on_date?(user, weekend_date)).to be(false)
+    end
+  end
+
+  describe '#weekend_registration_tooltip' do
+    let(:weekend_date) { Date.new(2026, 5, 17) }
+    let(:weekday_date) { Date.new(2026, 5, 18) }
+
+    it 'explains that automatic release is only for manager when user is blocked' do
+      user = User.new(role: 'funcionario', weekend_registration_allowed: false)
+
+      expect(helper.weekend_registration_tooltip(user, weekend_date)).to eq(
+        'Não liberado registrar ponto no fim de semana. Liberação automática somente para gestor.'
+      )
+    end
+
+    it 'returns an informational message for gestor on weekend' do
+      user = User.new(role: 'gestor', weekend_registration_allowed: false)
+
+      expect(helper.weekend_registration_tooltip(user, weekend_date)).to eq(
+        'Como gestor, a regra de bloqueio de fim de semana não se aplica ao seu perfil.'
+      )
+    end
+
+    it 'does not return tooltip on weekdays' do
+      user = User.new(role: 'funcionario', weekend_registration_allowed: false)
+
+      expect(helper.weekend_registration_tooltip(user, weekday_date)).to be_nil
+    end
+  end
 end
