dc1dbc3933fd2d9a2fe05acada15d9185c40c34b
Marco Castro <marcodotcastro@gmail.com>
2026-06-04 09:01:55 -0300

    feat: refine user preferences screen

--- app/controllers/user_preferences_controller.rb
+++ app/controllers/user_preferences_controller.rb
@@ -9,12 +9,8 @@ class UserPreferencesController < ApplicationController
     @user_preference = current_user.user_preference || current_user.build_user_preference
 
     if @user_preference.update(user_preference_params)
-      # Adicione log para depuração
-      Rails.logger.info "UserPreference atualizado com sucesso: #{@user_preference.attributes.inspect}"
       redirect_to edit_user_preference_path, notice: "Configurações atualizadas com sucesso."
     else
-      # Adicione log para erros
-      Rails.logger.error "Erro ao atualizar UserPreference: #{@user_preference.errors.full_messages.join(', ')}"
       render :edit, status: :unprocessable_entity
     end
   end
@@ -22,9 +18,6 @@ class UserPreferencesController < ApplicationController
   private
 
   def user_preference_params
-    # Imprima os parâmetros para depuração
-    Rails.logger.info "Parâmetros recebidos: #{params.inspect}"
-
     params.require(:user_preference).permit(
       :phone_number, :secondary_email, :language,
       :theme, :date_format, :time_format, :first_day_of_week,
